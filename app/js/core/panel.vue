1b5f12062d6fda16f17e1a511ec8798402df091b
jcy <523764061@qq.com>
2018-05-08 22:39:26 +0800

    add core floder

---
+++ app/js/core/panel.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+    <section :class="[panelClass,cname]">
+        <h4>{{ title }}</h4>
+        <slot/>
+    </section>
+</template>
+
+
+<script type="text/ecmascript-6">
+export default {
+    props: {
+        cname: {
+            type: String,
+            default: "",
+        },
+        title: {
+            type: String,
+            default: "",
+        },
+    },
+    data() {
+        return {
+            panelClass: "panel",
+        }
+    },
+}
+</script>
+
+<style lang="scss" module>
+    @import "../css/element.scss";
+    .panel {
+        @include panel;
+    }
+</style>
---
+++ app/js/core/slider.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+    <section :class="cname">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item in items" :key="item.href">
+                <router-link :to="{name:item.href}">
+                    <img src="item.src" alt="" >
+                </router-link>
+            </swiper-slide>
+            <div v-if="swiperOption.pagination" solt="pagination" class="swiper-pagination" />
+        </swiper>
+    </section>
+</template>
+
+
+<script type="text/ecmascript-6">
+import { swiper, swiperSlide } from "vue-awesome-swiper"
+export default {
+    components: {
+        swiper,
+        swiperSlide,
+    },
+    props: {
+        cname: {
+            type: String,
+            default: "",
+        },
+        swiperOption: {
+            type: Object,
+            default() {
+                return {
+                    autoPlay: true,
+                    loop: true,
+                    pagination: {
+                        el: ".swiper-pagination",
+                        type: "bullets",
+                    },
+                }
+            },
+        },
+        items: {
+            type: Array,
+            default() {
+                return [
+                    // { href: "", src: "" },
+                ]
+            },
+        },
+    },
+}
+</script>
+
+<style lang="scss" module>
+    @import '~swiper/dist/css/swiper.css';
+</style>
