8ea54c700fc9dd6ca4163e556e9c911c27dbb7a6
jcy <523764061@qq.com>
2018-05-15 00:28:09 +0800

    众筹-> rslider.vue daily.vue

---
+++ app/js/raise/index.vue
@@ -0,0 +1,39 @@
+/* 首页 组件 */
+<template lang="html">
+    <div>
+        <Heador/>
+        <Rslider/>
+        <Daily/>
+        <Footer cname="rfooter"/>
+        <Navbar/>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+import Heador from "../public/header.vue"
+import Footer from "../public/footer.vue"
+import Navbar from "../public/navbar.vue"
+import Rslider from "./rslider.vue"
+import Daily from "./daily.vue"
+export default {
+    components: {
+        Heador,
+        Rslider,
+        Daily,
+        Footer,
+        Navbar,
+    },
+}
+</script>
+
+<style lang="scss">
+    .rfooter {
+        margin-top: 0!important;
+        ul{
+            li:first-child{
+                display: none;
+            }
+        }
+    }
+</style>
+
