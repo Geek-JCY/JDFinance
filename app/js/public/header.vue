45d0fcf4c9b6831c9dbdf95dcea767b8d426c951
jcy <523764061@qq.com>
2018-05-15 17:50:55 +0800

    chapter-download ->开始

--- app/js/public/header.vue
+++ app/js/public/header.vue
@@ -1,7 +1,9 @@
 <template lang="html">
     <div :class="$style.header">
         <span :class="$style.left"><em>注册</em>&nbsp;|&nbsp;<em>登录</em></span>
-        <btn :class="$style.btnDownload">APP下载</btn>
+        <router-link :to="{name:'download'}">
+            <btn :class="$style.btnDownload">APP下载</btn>
+        </router-link>
     </div>
 </template>
 
---
+++ app/js/special/download.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+    <Panel :class="$style.panel" title="APP下载">
+        <section :class="$style.content">
+            <div/>
+            <p>已安装，点击这里直接打开</p>
+        </section>
+    </Panel>
+</template>
+
+<script type="text/ecmascript-6">
+import Panel from "../core/panel.vue"
+export default {
+    components: {
+        Panel,
+    },
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<style lang="scss" module>
+    @import "../../css/element.scss";
+    .panel {
+        @include panel;
+        margin: 0;
+        >h4 {
+            display: none;
+        }
+        .content {
+            @include flex;
+            width: 100%;
+            height: 1136px;
+            justify-content: center;
+            align-items: center;
+            >div{
+                background: url(https://img12.360buyimg.com/jrpmobile/jfs/t8956/285/1249806938/50853/8cd3c8f0/59b65f36N05367075.png?) center center no-repeat;
+                width: 600px;
+                height: 800px;
+                background-size: 100% auto;
+                margin-top: -400px;
+            }
+        }
+    }
+</style>
